57fe31c4dc91c28b93497212a24ff098f21e6c3b
klepa <pedro.klepa@gmail.com>
2020-07-08 10:44:12 -0300

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems);
+  M.Collapsible.init(elems);
 });
